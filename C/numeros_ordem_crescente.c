6462277694e011608af0481e8ff4310a6a423f44
TulioTeo <tulio.teo@gmail.com>
2022-08-19 23:52:14 -0300

    Programa que recebe 3 números e os colocam em ordem crescente

---
+++ C/numeros_ordem_crescente.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
+    int num1, num2, num3;
+
+    printf("\nPrograma que recebe 3 números inteiros e colocam em ordem crescente.\n");
+    printf("Digite o primeiro número: ");
+    scanf("%i",&num1);
+    printf("\nDigite o segundo número: ");
+    scanf("%i",&num2);
+    printf("\nDigite o terceiro número: ");
+    scanf("%i",&num3);
+
+    if(num1 <= num2 && num2 <= num3)
+    {
+        printf("A ordem crescente é: %i, %i, %i", num1, num2, num3); 
+    }
+    else if(num1 <= num3 && num3 <= num2)
+    {
+        printf("A ordem crescente é: %i, %i, %i", num1, num3, num2); 
+    }
+    else if(num2 <= num1 && num1 <= num3)
+    {
+        printf("A ordem crescente é: %i, %i, %i", num2, num1, num3);
+    }
+    else if(num2 <= num3 && num3 <= num1)
+    {
+        printf("A ordem crescente é: %i, %i, %i", num2, num3, num1);
+    }
+    else if(num3 <= num1 && num1 <= num2)
+    {
+        printf("A ordem crescente é: %i, %i, %i", num3, num1, num2);
+    }
+    else //num3 <= num2 && num2 <= num1
+    {
+        printf("A ordem crescente é: %i, %i, %i", num3, num2, num1);
+    }
+
+    return 0;
+}
